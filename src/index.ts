fcfaa8868d43875addf6796bc6a9387306f33850
Luke-zhang-04 <luke.zhang2004dev@gmail.com>
2020-08-09 17:08:45 -0400

    add starting program
    Signed-off-by: Luke-zhang-04 <luke.zhang2004dev@gmail.com>

---
+++ src/index.ts
@@ -0,0 +1,45 @@
+/**
+ * Luke Zhang's developer portfolio
+ * @copyright Copyright (C) 2020 Luke Zhang
+ * @author Luke Zhang Luke-zhang-04.github.io
+ * @license AGPL-3.0
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+import {Octokit} from "@octokit/core"
+import {token} from "../github.json"
+
+const octokit = new Octokit({auth: token});
+
+(async (): Promise<void> => {
+    const {repository} = await octokit.graphql(
+        `
+        {
+            repository(owner: "Luke-zhang-04", name: "DeStagnate") {
+                name
+                languages(first: 1, orderBy: {field: SIZE, direction: DESC}) {
+                    edges {
+                        node {
+                            name
+                            color
+                        }
+                    }
+                }
+            }
+        }
+        `,
+    )
+
+    console.log(JSON.stringify(repository))
+})()
