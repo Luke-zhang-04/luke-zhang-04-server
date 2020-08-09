40d168b3462e17ee5484fc203dd59374197f7364
Luke-zhang-04 <luke.zhang2004dev@gmail.com>
2020-08-09 19:06:20 -0400

    add ability to get github repo data
    Signed-off-by: Luke-zhang-04 <luke.zhang2004dev@gmail.com>

--- src/index.ts
+++ src/index.ts
@@ -17,37 +17,71 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
+
 import * as admin from "firebase-admin"
-import {Octokit} from "@octokit/core"
+import getRepoData from "./github"
+import parseUrl from "url-parse"
 import serviceAccount from "../admin-sdk.json"
-import {token} from "../github.json"
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
     databaseURL: "https://luke-zhang.firebaseio.com"
 })
 
-const db = admin.firestore(),
-    octokit = new Octokit({auth: token});
+const db = admin.firestore()
 
-(async (): Promise<void> => {
-    const {repository} = await octokit.graphql(
-        `
-        {
-            repository(owner: "Luke-zhang-04", name: "DeStagnate") {
-                name
-                languages(first: 1, orderBy: {field: SIZE, direction: DESC}) {
-                    edges {
-                        node {
-                            name
-                            color
-                        }
-                    }
-                }
-            }
-        }
-        `,
-    )
+/* eslint-disable @typescript-eslint/naming-convention */
+interface InitialProjectData {
+    date: number,
+    description: string,
+    file: string,
+    links: {
+        GitHub: string,
+        PyPi?: string,
+        NPM?: string,
+        live?: string,
+    },
+    tags: string[],
+    lang: {
+        name: string,
+        colour: string,
+    },
+}
+/* eslint-enable @typescript-eslint/naming-convention */
 
-    console.log(JSON.stringify(repository))
-})()
+interface ProjectData extends InitialProjectData {
+    name: string,
+}
+
+const getProjectData = (collection: string): Promise<ProjectData[]> => db
+        .collection(collection)
+        .get()
+        .then((snapshot) => {
+            const data: ProjectData[] = []
+
+            snapshot.forEach((doc) => {
+                data.push({
+                    ...doc.data() as InitialProjectData,
+                    name: doc.id,
+                })
+            })
+
+            return data
+        }),
+    getProjects = (): Promise<ProjectData[]> => Promise.all([
+        getProjectData("projects"),
+        getProjectData("collections")
+    ]).then((val) => [...val[0], ...val[1]])
+
+export const updateProjectValues = async (): Promise<void> => {
+    const data: ProjectData[] = await getProjects()
+
+    for (const project of data) {
+        const parsed = parseUrl(project.links.GitHub), // eslint-disable-next-line
+            projectName = parsed.pathname.split("/")[2]
+
+        console.log(getRepoData(projectName))
+    }
+}
+
+updateProjectValues()
