861d0a75577579b4cb02d6bb49a99c19bcdfee9e
Luke-zhang-04 <luke.zhang2004dev@gmail.com>
2020-08-11 20:03:44 -0400

    attempt deployment to Azure
    Signed-off-by: Luke-zhang-04 <luke.zhang2004dev@gmail.com>

--- TimerTrigger/index.js
+++
@@ -1,9 +0,0 @@
-module.exports = async (context, myTimer) => {
-    const timeStamp = new Date().toISOString()
-
-    if (myTimer.IsPastDue) {
-        context.log("JavaScript is running late!")
-    }
-    
-    context.log("JavaScript timer trigger function ran!", timeStamp)
-}
