0d3c087df9a96223ada306f622da325ade92fe0a
Luke-zhang-04 <Luke_zhang_04@protonmail.com>
2022-01-08 16:24:58 -0500

    update license

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -3,7 +3,7 @@
  *
  * @license BSD-3-Clause
  * @author Luke Zhang Luke-zhang-04.github.io
- * @copyright Copyright (c) 2020 - 2021 Luke Zhang
+ * @copyright Copyright (c) 2020 - 2022 Luke Zhang
  */
 
 export * as firebase from "./firebase"
